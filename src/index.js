7892be4a039ee056bad8f6f0388bcdd5ceff9df3
Felipe Palazzo <palazzofelipe@gmail.com>
2017-10-21 10:24:53 -0200

    Create root component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Root from './components/Root'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = {}
+
+ReactDOM.render(
+  <Root store={store} />,
+  document.getElementById('root')
+)
+
+registerServiceWorker()
