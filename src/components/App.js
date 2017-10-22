1322bd71fd613bf74a1a7c4bc5fcc7a4e8bccd2b
Felipe Palazzo <palazzofelipe@gmail.com>
2017-10-22 16:57:16 -0200

    Add app bar

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import Attendees from './Attendees'
 import { Route } from 'react-router-dom'
 import EditAttendee from './EditAttendee'
 import confirmationForm from './confirmationForm'
+import AppBar from 'material-ui/AppBar'
 
 const App = () =>
   <div>
+    <AppBar
+      title="Event List"
+    />
     <Route exact path="/" component={Attendees}/>
     <Route exact path="/attendees/:id" component={EditAttendee}/>
     <Route exact path="/attendees/:id/postcode" component={confirmationForm}/>
