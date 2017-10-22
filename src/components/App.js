9a6c28b9a0907142749a9d1454383c7a67e5d3ee
Felipe Palazzo <palazzofelipe@gmail.com>
2017-10-22 19:59:15 -0200

    Update top bar

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,15 @@ const App = () =>
     <AppBar
       title="Event List"
     />
-    <Route exact path="/" component={Attendees}/>
-    <Route exact path="/attendees/:id" component={EditAttendee}/>
-    <Route exact path="/attendees/:id/postcode" component={confirmationForm}/>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-10 col-md-offset-1 col-xs-12">
+          <Route exact path="/" component={Attendees}/>
+          <Route exact path="/attendees/:id" component={EditAttendee}/>
+          <Route exact path="/attendees/:id/postcode" component={confirmationForm}/>
+        </div>
+      </div>
+    </div>
   </div>
 
 export default App
